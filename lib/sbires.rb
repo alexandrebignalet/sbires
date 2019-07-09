eddde2e2a1da4064b95240c7ddb3ce8b3803808f
alexandrebignalet <alexandre.bignalet@gmail.com>
2019-07-09 18:05:52 +0200

    saving progress

--- lib/sbires.rb
+++ lib/sbires.rb
@@ -1,6 +1,85 @@
-require "sbires/version"
+require 'sbires/version'
+require 'securerandom'
 
 module Sbires
   class Error < StandardError; end
-  # Your code goes here...
+
+  LORD_NAMES = ["De Sinople", "D'Azure", "De Gueules", "D'Or", "D'Argent"]
+  MIN_PLAYERS_IN_GAME = 2
+  MAX_PLAYERS_IN_GAME = 5
+
+  class Game
+    attr_reader :players, :current_player
+
+    def initialize(players)
+      raise Error, "Not enough player to start the game" if players.length < MIN_PLAYERS_IN_GAME
+      raise Error, "Too much players to start the game" if players.length > MAX_PLAYERS_IN_GAME
+
+      @players = prepare_players(players)
+      @current_player = @players.sample.lord_name
+    end
+
+    private
+
+    def prepare_players(players)
+      remaining_lord_names = LORD_NAMES.dup
+      players.map {|name| Player.new(name, remaining_lord_names.shift)}
+    end
+  end
+
+  class Player
+    PEON_PER_PLAYER = 8
+
+    attr_reader :name, :lord_name, :peons
+
+    def initialize(name, lord_name)
+      @name = name
+      @lord_name = lord_name
+      @peons = PEON_PER_PLAYER
+    end
+
+    def place_peon_on(neighbour)
+      neighbour.receive_peon_from(lord_name)
+      @peons -= 1
+    end
+  end
+
+  class Neighbour
+    attr_reader :name
+
+    def initialize(name, players_in_game)
+      @name = name
+      @peons = []
+      @players_in_game = players_in_game
+    end
+
+    def receive_peon_from(lord_name)
+      raise Error, "Neighbour is full" if full?
+
+      @peons << Peon.new(lord_name)
+    end
+
+    def full?
+      @peons.length >= current_peon_limit
+    end
+
+    private
+
+    def current_peon_limit
+      peon_limits = {
+          MIN_PLAYERS_IN_GAME => 4,
+          3 => 5,
+          4 => 7,
+          MAX_PLAYERS_IN_GAME => 8
+      }
+
+      peon_limits[@players_in_game]
+    end
+  end
+
+  class Peon
+    def initialize(belongs_to)
+      @belongs_to = belongs_to
+    end
+  end
 end
--- spec/sbires_spec.rb
+++ spec/sbires_spec.rb
@@ -1,9 +1,91 @@
-RSpec.describe Sbires do
-  it "has a version number" do
-    expect(Sbires::VERSION).not_to be nil
-  end
+module Sbires
+  RSpec.describe Sbires do
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    it "should not be launch with less than 2 player or more than 5" do
+      expect { Game.new([]) }.to raise_error Error
+      expect { Game.new(["player1"]) }.to raise_error Error
+      expect { Game.new(["player1", "player2", "player3", "player4", "player5", "player6"]) }.to raise_error Error
+    end
+
+    context "game start" do
+      let(:player_names) { %w(Jean Francois) }
+
+      before do
+        @game = Game.new(player_names)
+        @players = @game.players
+        @player_one = @players.first
+        @player_two = @players.last
+      end
+
+      it "game should create players" do
+        expect(@players.map(&:name)).to eq(player_names)
+      end
+
+      it "game should elect the first player" do
+        expect(@game.current_player).not_to be nil
+        expect(@players.map(&:lord_name)).to include(@game.current_player)
+      end
+
+      it "game should assign a lord name for each players" do
+        puts @players.map(&:lord_name)
+        expect(LORD_NAMES).to include(@players_one.lord_name)
+        expect(LORD_NAMES).to include(@players_two.lord_name)
+      end
+    end
+
+    context "Neighbour" do
+
+        it "a player should be allowed to place peon on a neighbour" do
+          neighbour = Neighbour.new("Taverne", 2)
+          player = Player.new("Jean", LORD_NAMES[0])
+
+          player.place_peon_on(neighbour)
+
+          expect(neighbour.full?).to be false
+          expect(player.peons).to be Player::PEON_PER_PLAYER - 1
+        end
+
+        it "neighbour should be full with 4 peons when 2 players in game" do
+          player = Player.new("Jean", LORD_NAMES[0])
+          neighbour = Neighbour.new("Taverne", 2)
+
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+
+          expect(neighbour.full?).to be true
+        end
+
+        it "neighbour should raise if player try to put a peon on a full neighbour" do
+          player = Player.new("Jean", LORD_NAMES[0])
+          neighbour = Neighbour.new("Taverne", 2)
+
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          expect { player.place_peon_on(neighbour) }.to raise_error Error
+        end
+
+        it "neightbour should be full with 5 peons when 3 players in game" do
+          player = Player.new("Jean", LORD_NAMES[0])
+          neighbour = Neighbour.new("Taverne", 3)
+
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+          player.place_peon_on(neighbour)
+
+          expect(neighbour.full?).to be true
+        end
+      end
+
+    context "Game peon placement" do
+      it "game should not allow a player to place multiple peons consecutively" do
+
+      end
+    end
   end
 end
