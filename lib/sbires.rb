41d7922e84800075583ac146b94bcefc7ae21942
alexandrebignalet <alexandre.bignalet@gmail.com>
2019-07-10 10:47:51 +0200

    created card system, added pick card on pawn placed

--- lib/sbires.rb
+++ lib/sbires.rb
@@ -9,29 +9,33 @@ module Sbires
   MAX_PLAYERS_IN_GAME = 5
 
   class Game
-    attr_reader :players, :current_player
+    attr_reader :players, :current_player_name, :neighbours
 
     def initialize(players)
       raise Error, "Not enough player to start the game" if players.length < MIN_PLAYERS_IN_GAME
       raise Error, "Too much players to start the game" if players.length > MAX_PLAYERS_IN_GAME
 
       @players = prepare_players(players)
-      @current_player = @players.sample.lord_name
+      @current_player_name = @players.sample.lord_name
       @neighbours = create_neighbours
     end
 
     def place_pawn(lord_name, neighbour_name)
-      player = players.detect { |p| p.lord_name == lord_name }
+      player = player(lord_name)
       raise Error, "Player #{lord_name} unknown" if player.nil?
-      raise Error, "Not your turn" unless player.lord_name == current_player
+      raise Error, "Not your turn" unless player.lord_name == current_player_name
       neighbour = @neighbours.detect { |n| n.name == neighbour_name }
       raise Error, "Neighbour #{neighbour_name} unknown" if neighbour.nil?
 
       player.place_pawn_on(neighbour)
+      player.pick_card_from(neighbour)
 
       next_player
     end
 
+    def player(lord_name)
+      players.detect { |p| p.lord_name == lord_name }
+    end
     private
 
     def create_neighbours
@@ -39,11 +43,11 @@ module Sbires
     end
 
     def next_player
-      @current_player = players[next_player_index]
+      @current_player_name = players[next_player_index]
     end
 
     def next_player_index
-      index_of_current_player = players.index { |p| p.lord_name == current_player }
+      index_of_current_player = players.index { |p| p.lord_name == current_player_name }
       index_of_current_player == players.length ? 0 : ++index_of_current_player
     end
 
@@ -57,33 +61,68 @@ module Sbires
     PAWN_PER_PLAYER = 8
     INITIAL_POINT_NUMBER = 5
 
-    attr_reader :name, :lord_name, :pawns, :points
+    attr_reader :name, :lord_name, :pawns, :points, :cards
 
     def initialize(name, lord_name)
       @name = name
       @lord_name = lord_name
       @points = INITIAL_POINT_NUMBER
       @pawns = (0...PAWN_PER_PLAYER).map { Pawn.new(@lord_name) }
+      @cards = []
     end
 
     def place_pawn_on(neighbour)
-      raise Error, "No more pawns to place" if pawns.length == 0
+      raise Error, "No more pawns to place" if all_pawns_placed?
       neighbour.receive_pawn_from(pawns.shift)
+    end
+
+    def pick_card_from(neighbour)
+      @cards << neighbour.shift_deck
+    end
+
+    def discard_in(card, neighbour)
+      raise Error, "Not your card" unless cards.include? card
+
+      neighbour.add_discarded(card)
+      @cards.delete card
+    end
+
+    private
+
+    def all_pawns_placed?
+      pawns.length == 0
+    end
+  end
+  class Pawn
+    def initialize(belongs_to)
+      @belongs_to = belongs_to
     end
   end
 
   class Neighbour
-    attr_reader :name
+    CARD_NUMBER_PER_NEIGHBOUR = 26
+
+    attr_reader :name, :deck, :discard
 
     def initialize(name, players_in_game)
       @name = name
       @pawns = []
       @players_in_game = players_in_game
+      @deck = (0...CARD_NUMBER_PER_NEIGHBOUR).map { Card.new(@name) }
+      @discard = []
     end
 
     def receive_pawn_from(pawn)
       raise Error, "Neighbour is full" if full?
       @pawns << pawn
+    end
+
+    def shift_deck
+      @deck.shift
+    end
+
+    def add_discarded(card)
+      @discard << card
     end
 
     def full?
@@ -104,9 +143,11 @@ module Sbires
     end
   end
 
-  class Pawn
-    def initialize(belongs_to)
-      @belongs_to = belongs_to
+  class Card
+    attr_reader :neighbour_name
+
+    def initialize(neighbour_name)
+      @neighbour_name = neighbour_name
     end
   end
 end
--- spec/sbires_spec.rb
+++ spec/sbires_spec.rb
@@ -22,8 +22,8 @@ module Sbires
       end
 
       it "game should elect the first player" do
-        expect(@game.current_player).not_to be nil
-        expect(@players.map(&:lord_name)).to include(@game.current_player)
+        expect(@game.current_player_name).not_to be nil
+        expect(@players.map(&:lord_name)).to include(@game.current_player_name)
       end
 
       it "game should assign a lord name for each players" do
@@ -39,69 +39,115 @@ module Sbires
 
     context "Neighbour" do
 
-        it "a player should be allowed to place pawn on a neighbour" do
-          neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 2)
-          player = Player.new("Jean", LORD_NAMES[0])
+      it "a player should be allowed to place pawn on a neighbour" do
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 2)
+        player = Player.new("Jean", LORD_NAMES.first)
 
-          player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
 
-          expect(neighbour.full?).to be false
-          expect(player.pawns.length).to be Player::PAWN_PER_PLAYER - 1
-        end
-
-        it "should raise when player has no more pawns to place" do
-          chateau = Neighbour.new(NEIGHBOURS_NAMES.first, 3)
-          taverne = Neighbour.new(NEIGHBOURS_NAMES[1], 3)
-          player = Player.new("Jean", LORD_NAMES.first)
-
-          player.place_pawn_on(chateau)
-          player.place_pawn_on(chateau)
-          player.place_pawn_on(chateau)
-          player.place_pawn_on(chateau)
-          player.place_pawn_on(taverne)
-          player.place_pawn_on(taverne)
-          player.place_pawn_on(taverne)
-          player.place_pawn_on(taverne)
-
-          expect { player.place_pawn_on(taverne) }.to raise_error Error
-        end
-
-        it "neighbour should be full with 4 pawns when 2 players in game" do
-          player = Player.new("Jean", LORD_NAMES[0])
-          neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 2)
-
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-
-          expect(neighbour.full?).to be true
-        end
-
-        it "neighbour should raise if player try to put a pawn on a full neighbour" do
-          player = Player.new("Jean", LORD_NAMES[0])
-          neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 2)
-
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          expect { player.place_pawn_on(neighbour) }.to raise_error Error
-        end
-
-        it "neightbour should be full with 5 pawns when 3 players in game" do
-          player = Player.new("Jean", LORD_NAMES[0])
-          neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 3)
-
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-          player.place_pawn_on(neighbour)
-
-          expect(neighbour.full?).to be true
-        end
+        expect(neighbour.full?).to be false
+        expect(player.pawns.length).to be Player::PAWN_PER_PLAYER - 1
       end
+
+      it "should raise when player has no more pawns to place" do
+        chateau = Neighbour.new(NEIGHBOURS_NAMES.first, 3)
+        taverne = Neighbour.new(NEIGHBOURS_NAMES[1], 3)
+        player = Player.new("Jean", LORD_NAMES.first)
+
+        player.place_pawn_on(chateau)
+        player.place_pawn_on(chateau)
+        player.place_pawn_on(chateau)
+        player.place_pawn_on(chateau)
+        player.place_pawn_on(taverne)
+        player.place_pawn_on(taverne)
+        player.place_pawn_on(taverne)
+        player.place_pawn_on(taverne)
+
+        expect { player.place_pawn_on(taverne) }.to raise_error Error
+      end
+
+      it "neighbour should be full with 4 pawns when 2 players in game" do
+        player = Player.new("Jean", LORD_NAMES.first)
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 2)
+
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+
+        expect(neighbour.full?).to be true
+      end
+
+      it "neighbour should raise if player try to put a pawn on a full neighbour" do
+        player = Player.new("Jean", LORD_NAMES.first)
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 2)
+
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        expect { player.place_pawn_on(neighbour) }.to raise_error Error
+      end
+
+      it "neighbour should be full with 5 pawns when 3 players in game" do
+        player = Player.new("Jean", LORD_NAMES.first)
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 3)
+
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        player.place_pawn_on(neighbour)
+        
+        expect(neighbour.full?).to be true
+      end
+
+      it "should create a deck with 26 cards of the neighbour name" do
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 3)
+
+        expect(neighbour.deck).to all( have_attributes neighbour_name: neighbour.name )
+        expect(neighbour.deck.length).to eq Neighbour::CARD_NUMBER_PER_NEIGHBOUR
+      end
+
+      it "should create an empty discard on init" do
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 3)
+
+        expect(neighbour.discard.length).to eq 0
+      end
+
+      it "should allow player to shift a card of the deck" do
+        player = Player.new("Jean", LORD_NAMES.first)
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 3)
+
+        player.pick_card_from(neighbour)
+
+        expect(player.cards.length).to eq 1
+        expect(player.cards.first).to have_attributes neighbour_name: neighbour.name
+        expect(neighbour.deck.length).to eq Neighbour::CARD_NUMBER_PER_NEIGHBOUR - 1
+      end
+
+      it "should allow player to discard a card" do
+        player = Player.new("Jean", LORD_NAMES.first)
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 3)
+
+        player.pick_card_from(neighbour)
+        player.discard_in(player.cards.first, neighbour)
+
+        expect(player.cards.length).to eq 0
+        expect(neighbour.deck.length).to eq Neighbour::CARD_NUMBER_PER_NEIGHBOUR - 1
+        expect(neighbour.discard.length).to eq 1
+      end
+
+      it "should raise if discarded card does not belong to the player" do
+        player = Player.new("Jean", LORD_NAMES.first)
+        player2 = Player.new("Mich", LORD_NAMES[2])
+        neighbour = Neighbour.new(NEIGHBOURS_NAMES.sample, 3)
+
+        player2.pick_card_from(neighbour)
+        expect { player.discard_in(player2.cards.first, neighbour) }.to raise_error Error
+      end
+    end
+
 
     context "Game pawn placement" do
       before do
@@ -109,12 +155,23 @@ module Sbires
         @players = @game.players
         @player_one = @players.first
         @player_two = @players.last
+
+        @current_player_lord_name = @game.current_player_name
+        @game.place_pawn(@current_player_lord_name, NEIGHBOURS_NAMES.first)
       end
 
       it "game should not allow a player to place multiple pawns consecutively" do
-        current_player_lord = @game.current_player
-        @game.place_pawn(current_player_lord, NEIGHBOURS_NAMES.first)
-        expect { @game.place_pawn(current_player_lord, NEIGHBOURS_NAMES.first) }.to raise_error Error
+        expect { @game.place_pawn(@current_player_lord_name, NEIGHBOURS_NAMES.first) }.to raise_error Error
+      end
+
+      it "after pawn placement player should pick a card on its neighbour" do
+        current_player = @game.player(@current_player_lord_name)
+        expect(current_player.cards.length).to eq 1
+        expect(current_player.cards.first.neighbour_name).to eq NEIGHBOURS_NAMES.first
+
+        neighbour = @game.neighbours.detect { |n| n.name == NEIGHBOURS_NAMES.first }
+        expect(neighbour.deck.length).to eq Neighbour::CARD_NUMBER_PER_NEIGHBOUR - 1
+        expect(neighbour.discard.length).to eq 0
       end
     end
   end
